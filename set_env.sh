243cb812adf0a62e8a8ad3583f88bcbb7cdd7e44
GangpanX <pg994148509@gmail.com>
2026-07-27 22:21:13 +0800

    Update

--- set_env.sh
+++ set_env.sh
@@ -1,5 +1,5 @@
-export HGCN_HOME=$(pwd)
-export LOG_DIR="$HGCN_HOME/logs"
-export PYTHONPATH="$HGCN_HOME:${PYTHONPATH:-}"
+export HTSG_HOME=$(pwd)
+export LOG_DIR="$HTSG_HOME/logs"
+export PYTHONPATH="$HTSG_HOME:${PYTHONPATH:-}"
 export DATAPATH="./data_path"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-9.0/lib64
